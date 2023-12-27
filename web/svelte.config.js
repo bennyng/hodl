36a3c2086279dcc870c2ac70de6577ed88d7d6c8
B <>
2023-12-27 23:18:29 +0800

    test GH pages

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,18 +1,32 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
+const isGHPages = process.env.GITHUB_PAGES == "true";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
+  kit: isGHPages
+    ? {
+        adapter: adapter(),
 
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-  },
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte",
+
+        appDir: "_app",
+        paths: {
+          base: "/hodl",
+        },
+      }
+    : {
+        adapter: adapter(),
+
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: "#svelte",
+      },
 };
 
 export default config;
